44d0e7f287444b9064da1c47fa434b37c6f09d85
Zachary Chapman-Cueco <chapmancueco@gmail.com>
2022-04-15 13:41:05 -0400

    codewars challenge, my solution

---
+++ cw13.js
@@ -0,0 +1,47 @@
+// Fundamentals: Return 8kyu
+
+/*Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+*/
+//PSUEDO CODE
+//Seems like a refresher on math operations, should be easy enough
+
+//MY ATTEMPT
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return a ** b
+}
+    
+function subt(a,b){
+    return a - b
+} //yep, easy refresher
