5977688241bd3d43cea1517d422f5206e1d94764
Zachary Chapman-Cueco <chapmancueco@gmail.com>
2022-04-25 17:54:01 -0400

    codewars challenge, my solution

---
+++ cw19.js
@@ -0,0 +1,11 @@
+//Bin to Decimal 8kyu
+
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//PSUEDO CODE
+//Should be pretty simple, learnt about parseInt, that should do it
+
+//MY ATTEMPT
+function binToDec(bin){
+    return parseInt(bin, 2)
+  }
